7a882c8fd21559673da74960d96f17e35a516f40
benho <gz21472@bristol.ac.uk>
2022-10-07 23:20:24 +0100

    opa

--- part1/testClient.go
+++ part1/testClient.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
 
+func read(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	msg, _ := reader.ReadString('\n')
+	fmt.Printf(msg)
+}
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	conn, _ := net.Dial("tcp", "127.0.0.1:8030")
+	for {
+		fmt.Printf("Enter msg:\t")
+		msg, _ := stdin.ReadString('\n')
+		_, _ = fmt.Fprintln(conn, msg)
+		read(conn)
+	}
 }
--- part1/testServer.go
+++ part1/testServer.go
@@ -6,10 +6,17 @@ import (
 	"net"
 )
 
-func main() {
-	ln, _ := net.Listen("tcp", ":8030")
-	conn, _ := ln.Accept()
+func handleConnection(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	msg, _ := reader.ReadString('\n')
 	fmt.Println(msg)
+	fmt.Fprintln(conn, "OK")
+}
+
+func main() {
+	ln, _ := net.Listen("tcp", ":8030")
+	for {
+		conn, _ := ln.Accept()
+		go handleConnection(conn)
+	}
 }
