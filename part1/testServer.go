38a805a92295555db3b8fcf22179f927d016edce
benho <gz21472@bristol.ac.uk>
2022-10-09 01:15:53 +0100

    everything is good

--- part1/testServer.go
+++ part1/testServer.go
@@ -8,9 +8,11 @@ import (
 
 func handleConnection(conn net.Conn) {
 	reader := bufio.NewReader(conn)
-	msg, _ := reader.ReadString('\n')
-	fmt.Println(msg)
-	fmt.Fprintln(conn, "OK")
+	for {
+		msg, _ := reader.ReadString('\n')
+		fmt.Println(msg)
+		fmt.Fprintln(conn, "OK")
+	}
 }
 
 func main() {
--- server/server.go
+++ server/server.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"flag"
-	"net"
 	"fmt"
+	"net"
 )
 
 type Message struct {
@@ -13,22 +13,39 @@ type Message struct {
 }
 
 func handleError(err error) {
-	// TODO: all
 	// Deal with an error event.
+	if err != nil {
+		panic(err)
+	}
 }
 
 func acceptConns(ln net.Listener, conns chan net.Conn) {
-	// TODO: all
 	// Continuously accept a network connection from the Listener
 	// and add it to the channel for handling connections.
+	for {
+		conn, err := ln.Accept()
+		handleError(err)
+		conns <- conn
+	}
 }
 
 func handleClient(client net.Conn, clientid int, msgs chan Message) {
-	// TODO: all
 	// So long as this connection is alive:
 	// Read in new messages as delimited by '\n's
 	// Tidy up each message and add it to the messages channel,
 	// recording which client it came from.
+	reader := bufio.NewReader(client)
+	for {
+		in, err := reader.ReadString('\n')
+		if err != nil {
+			client.Close()
+			break
+		}
+		text := fmt.Sprintf("[%d]: %s", clientid, in)
+		msg := Message{sender: clientid, message: text}
+		msgs <- msg
+	}
+
 }
 
 func main() {
@@ -37,7 +54,9 @@ func main() {
 	portPtr := flag.String("port", ":8030", "port to listen on")
 	flag.Parse()
 
-	//TODO Create a Listener for TCP connections on the port given above.
+	//Create a Listener for TCP connections on the port given above.
+	ln, err := net.Listen("tcp", *portPtr)
+	handleError(err)
 
 	//Create a channel for connections
 	conns := make(chan net.Conn)
@@ -45,19 +64,24 @@ func main() {
 	msgs := make(chan Message)
 	//Create a mapping of IDs to connections
 	clients := make(map[int]net.Conn)
+	//Create integer to track current ID to use
+	currentID := 0
 
 	//Start accepting connections
 	go acceptConns(ln, conns)
 	for {
 		select {
 		case conn := <-conns:
-			//TODO Deal with a new connection
-			// - assign a client ID
-			// - add the client to the clients channel
-			// - start to asynchronously handle messages from this client
+			clients[currentID] = conn
+			go handleClient(conn, currentID, msgs)
+			currentID++
+
 		case msg := <-msgs:
-			//TODO Deal with a new message
-			// Send the message to all clients that aren't the sender
+			for clientID, client := range clients {
+				if msg.sender != clientID {
+					fmt.Fprintf(client, msg.message)
+				}
+			}
 		}
 	}
 }
